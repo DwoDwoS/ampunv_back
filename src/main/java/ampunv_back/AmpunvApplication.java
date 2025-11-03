be9f90ff8fb93d441aeb2568a0c56bdc54f0130f
Elouan <elouangauriaud@gmail.com>
2025-11-03 12:03:22 +0100

    initializing project with connection to neon

---
+++ src/main/java/ampunv_back/AmpunvApplication.java
@@ -0,0 +1,13 @@
+package ampunv_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmpunvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmpunvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ampunv_back/AmpunvApplicationTests.java
@@ -0,0 +1,13 @@
+package ampunv_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmpunvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
