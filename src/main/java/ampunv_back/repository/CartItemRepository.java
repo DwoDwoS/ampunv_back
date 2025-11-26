50bf851cbac440bd63ba84ee3e5a1129aa0cc0a1
Elouan <elouangauriaud@gmail.com>
2025-11-26 10:35:35 +0100

    wip: still need to test webhooks

--- src/main/java/ampunv_back/repository/CartItemRepository.java
+++ src/main/java/ampunv_back/repository/CartItemRepository.java
@@ -1,17 +1,16 @@
 package ampunv_back.repository;
 
 import ampunv_back.entity.CartItem;
+import ampunv_back.entity.User;
+import ampunv_back.entity.Furniture;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
-@Repository
+import java.util.List;
+import java.util.Optional;
+
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-    @Transactional
-    @Modifying
-    @Query("DELETE FROM CartItem c WHERE c.furniture.id = :furnitureId")
-    int deleteByFurnitureId(@Param("furnitureId") Long furnitureId);
+    List<CartItem> findByUser(User user);
+    Optional<CartItem> findByUserAndFurniture(User user, Furniture furniture);
+    void deleteByUser(User user);
+    void deleteByFurnitureId(Long furnitureId);
 }
